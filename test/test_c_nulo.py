f0f193ac7ffb6d926b3ccf072be69651caa7fc98
dfleta <gelpiorama@gmail.com>
2020-10-20 18:50:17 +0200

    feat(pytest.ini): Añadir marker a los test caso c nulo
    Añadir comentario al marker c_nulo y modificar el nombre de los test

--- test/test_c_nulo.py
+++ test/test_c_nulo.py
@@ -8,7 +8,5 @@ def test_c_nulo():
     assert raiz_ecuacion_segundo_grado(1, 1, 0) == (0, -1)
 
 @pytest.mark.c_nulo
-def test_c_nulo():
+def test_c_nulo_raiz_nula():
     assert raiz_ecuacion_segundo_grado(2, 4, 0) == (0, -2)
-
-

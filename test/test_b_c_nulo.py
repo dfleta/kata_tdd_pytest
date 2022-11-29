e79296e1071f59791387182e99a4ace40ab6a97e
dfleta <gelpiorama@gmail.com>
2022-11-29 12:37:47 +0100

    test(test_b_c_nulo): Incluir test a nulo con b y c nulos

--- test/test_b_c_nulo.py
+++ test/test_b_c_nulo.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.mark.b_c_nulo
 def test_raiz_nula_unica():
     assert raiz_ecuacion_segundo_grado(1, 0, 0) == 0
+
+
+@pytest.mark.b_c_nulo
+def test_raiz_nula_unica_a_nulo():
+    assert raiz_ecuacion_segundo_grado(0, 0, 0) == None
